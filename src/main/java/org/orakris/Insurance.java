3c6f7d02bd9a1b42df12939b10d576c50ef3733d
tourist-dev <shaik.sameer@orakris.com>
2021-05-26 08:36:58 +0530

    Added Insurance.java

---
+++ src/main/java/org/orakris/Insurance.java
@@ -0,0 +1,45 @@
+package org.orakris;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Insurance {
+    @Id
+    private int id;
+    private String name;
+    private int tenure;
+    private int amount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTenure() {
+        return tenure;
+    }
+
+    public void setTenure(int tenure) {
+        this.tenure = tenure;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
