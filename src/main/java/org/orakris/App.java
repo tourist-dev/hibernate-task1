522ab5795506fa5de2c99b4f19f42fbda3c34509
tourist-dev <shaik.sameer@orakris.com>
2021-05-26 09:06:22 +0530

    moved hibernate.cfg.xml to resources folder and added code to insert a record into table Insurance in App.java

--- src/main/java/org/orakris/App.java
+++ src/main/java/org/orakris/App.java
@@ -1,5 +1,10 @@
 package org.orakris;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -13,6 +18,12 @@ public class App
         insurance.setName("vehicle policy");
         insurance.setAmount(100000);
         insurance.setTenure(10);
-        
+
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(Insurance.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(insurance);
+        tx.commit();
     }
 }
